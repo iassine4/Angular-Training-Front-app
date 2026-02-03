ccfc8baccc8bc83076fcdba67331da5c20c564c0
Yassine <yassine.zriba@fms-ea.com>
2026-02-03 14:59:50 +0100

    feat(api): ajouter un model Training et TrainingApiService

---
+++ src/app/serveices/training-api.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Training } from '../models/training.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrainingApiService {  
+
+    private apiUrl = '/api/trainings';
+    
+    constructor(private http: HttpClient) { }
+
+    public getTrainings(){
+        return this.http.get<Training[]>(`${this.apiUrl}/trainings`);
+    }
+
+    public getTrainingById(id: number){
+        return this.http.get<Training>(`${this.apiUrl}/${id}`);
+    }
+}
