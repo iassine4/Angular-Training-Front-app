cbe06386122cf1d2ddbfa2f6c3251b6e9c6db503
Yassine <yassine.zriba@fms-ea.com>
2026-02-05 15:22:19 +0100

    init: creation du composant customer-form

---
+++ src/app/components/forms/customer-form/customer-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-costumer-form',
+  imports: [],
+  templateUrl: './customer-form.html',
+  styleUrl: './customer-form.css',
+})
+export class CustomerForm {
+
+}
