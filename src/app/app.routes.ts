414abf6f093ce0cec6cd4209f9c8a50e603c9404
Yassine <yassine.zriba@fms-ea.com>
2026-02-09 08:35:17 +0100

    fix(route): utiliser la navigation absolue vers le panier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,32 @@
 import { Routes } from '@angular/router';
-
-import { UserForm } from './components/forms/user-form/user-form';
 import { PageNotFound } from './components/error-page/page-not-found/page-not-found';
 import { LoginComponent } from './components/login/login/loginComponent';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'trainings', pathMatch: 'full' },
+  { path: '', redirectTo: 'trainings', pathMatch: 'full' },
 
-    { 
-        path: 'login',
-        component: LoginComponent
-    },
-    {   // route protégée : seul un user connecté accède au panier/commande
-        path: 'cart',
-        canActivate: [authGuard],
-        loadComponent: () => import('./components/cart/cartComponent').then(m => m.CartComponent)
-    },
+  { path: 'login', component: LoginComponent },
 
-    {
-        path : 'trainings',
-        loadComponent: () => import('./components/trainings/trainingsComponent').then(m => m.TrainingsComponent)
-    },
-    {
-        path: 'form',
-        component: UserForm
-    },
-    { path: 'form', canActivate: [authGuard], loadComponent: () => import('./components/forms/customer-form/customer-form').then(m => m.CustomerFormComponent) },
+  {
+    path: 'trainings',
+    loadComponent: () =>
+      import('./components/trainings/trainingsComponent').then((m) => m.TrainingsComponent),
+  },
 
-    //toute route inconnue redirige vers /page-not-found
-    { path: '**', component: PageNotFound },
+  // Panier accessible (plus fluide)
+  {
+    path: 'cart',
+    loadComponent: () => import('./components/cart/cartComponent').then((m) => m.CartComponent),
+  },
+
+  // Commande (Customer) protégée
+  {
+    path: 'form',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./components/forms/customer-form/customer-form').then((m) => m.CustomerFormComponent),
+  },
+
+  { path: '**', component: PageNotFound },
 ];
